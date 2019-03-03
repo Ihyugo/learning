93d416b7e3b4c57224e0a2665057b95d34746f87
yugo ikuta <yugo1997.10.14@gmail.com>
2019-03-03 14:02:13 +0900

    整理

---
+++ Adline.py
@@ -0,0 +1,67 @@
+import numpy as np
+class AdalineGD(object):
+    """パーセプトロンの分類器
+
+    パラメータ
+    ----------
+    eta : float
+        学習率 (0.0より大きく1.0以下の値)
+    n_iter : int
+        トレーニングデータのトレーニング回数
+    属性
+    ----------
+    w_ : 1次元配列
+        適合後の重み
+    errors_ : リスト
+        各エポックでの誤分類数
+    """
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        """トレーニングデータに適合させる
+
+        パラメータ
+        ---------
+        X : {配列のようなデータ構造},shape = {n_samples, n_features}
+            トレーニングデータ
+            n_sampleはサンプルの個数、n_featureは特徴量の個数
+        Y : 配列のようなデータ構造、 shape = {n_samples}
+            目的変数
+
+            戻り値
+        ---------
+        self: object
+
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for _ in range(self.n_iter): # トレーニング回数分トレーニングデータを反復
+            #活性化関数の出力の計算　Φ(wtx) = wtx
+            output = self.net_input(X)
+            #誤差、yi - Φ(zi)の計算
+            errors = (y - output)
+            #重み　w1,,,,wmの更新
+            #Δwj = η(yi - y'i)xji (j = 1,,,,m)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            #　重みw0の更新：Δw- = η(yi - y'i)
+            self.w_[0] += self.eta * errors.sum()
+            #コスト関数の計算　J(w) = 0.5*∑(yi - Φ(zi))^2
+            cost = (errors**2).sum() / 2.0
+            # コストの格納
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """総入力を計算"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activatation(self, X):
+        """線形活性化関数の出力を計算"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """1ステップ後のクラスラベルを返す"""
+        return np.where(self.acrivation(X) >= 0.0, 1, -1)
--- python_line.py
+++ python_line.py
@@ -1,3 +1,4 @@
+# text p.28
 import pandas as pd
 df = pd.read_csv('https://archive.ics.uci.edu/ml/'
                 'machine-learning-databases/iris/iris.data', header=None)
@@ -19,6 +20,7 @@ plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.show()
 
+#text p.29
 #凡例の設定（左上に配置）
 from Perceptron import Perceptron
 #パーセプロトンのオブジェクトの作製（インスタンス化）
@@ -32,4 +34,39 @@ plt.ylabel('Numbet of misclassifications')
 # 図の表示
 plt.legend(loc='upper left')
 #図の表示
+plt.show()
+
+#text p.31
+#決定境界のプロット
+from iris_marker import *
+plot_decision_regions(X, y, classifier=ppn)
+#軸のラベル設定
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+#テキスト　p.37
+
+from Adline import *
+#勾配領域を１行２勾配領域を１行２列に分割
+fig, ax =  plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+#勾配降下法によるADLINEの学習(学習率　eta=0.01)
+ada1 = AdalineGD(n_iter=10, eta=0.01,).fit(X,y)
+#エポック数とコストの関係を表す折れ線グラフのプロット（縦軸のコストは常用対数）
+ax[0].plot(range(1,len(ada1.cost_)+1), np.log10(ada1.cost_), marker='o')
+#軸のラベルの設定
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error')
+#タイトルの設定
+ax[0].set_title('Adaline - Learning-error')
+#勾配降下法によるADLINEの学習（学習率　eta=0.0001)
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+#エポック数うとコストの関係を表す折れ線グラフのプロット
+ax[1].plot(range(1,len(ada2.cost_)+1), ada2.cost_, marker='o')
+#軸のラベルの設定
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+#タイトルの設定
+ax[1].set_title('Adaline - Learning rate 0.0001')
 plt.show()
