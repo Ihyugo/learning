a0351d03d20800551b8a010fff8731c5548985f2
yugo ikuta <yugo1997.10.14@gmail.com>
2019-03-08 10:16:49 +0900

    change

--- 3doc/python_line.py
+++ 3doc/python_line.py
@@ -10,7 +10,8 @@ X = iris.data[:, [2, 3]]
 # クラスラベルを取得
 y = iris.target
 print("Class labels:", np.unique(y))
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+# from sklearn.cross_validation import train_test_split
 #トレーニングデータとテストデータに分割
 # 全体の３０％をテストデータにする
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
@@ -26,7 +27,7 @@ X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 from sklearn.linear_model import Perceptron
 # エポック数4.0、学習率0.1でパーセプトロンのインスタンスを生成
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0, shuffle=True)
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0, shuffle=True)
 # トレーニングデータをモデルに適合させる
 ppn.fit(X_train_std, y_train)
 #　テストデータで予測を実施
@@ -53,4 +54,96 @@ plt.ylabel('petal width [standardized]')
 # 凡例の設定
 plt.legend(loc = 'upper left')
 # 図の表示
+plt.show()
+
+# text p.60
+
+from sklearn.linear_model import LogisticRegression
+#　ロジスティック回帰のインスタンスを生成
+lr = LogisticRegression(C=1000.0, random_state=0)
+#　トレーニングデータをモデルに適合させる
+lr.fit(X_train_std, y_train)
+#　決定境界をプロット
+plot_decision_regions(X_combined_std,y_combined, classifier=lr, test_idx=range(105,150))
+plt.xlabel('petal length [standardized')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+# text p.65
+
+#　空のリストを生成（重み係数、　逆正則化パラメータ）
+weights, params = [], []
+# １０個の逆正則化パラメータに対応するロジスティック回帰モデルをそれぞれ処理
+for c in np.arange(-5, 5):
+    update = 10.**c
+    lr = LogisticRegression(C=update, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(update)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='-', label='petal length')
+plt.ylabel('weight coefficient')
+plt.xlabel('c')
+plt.legend(loc='upper left')
+plt.xscale('log')
+plt.show()
+
+# text p.69
+
+from sklearn.svm import SVC
+svm = SVC(kernel='linear', C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std,y_combined,classifier=svm,test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+# text p.71-72
+
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+plt.scatter(X_xor[y_xor == 1, 0], X_xor[y_xor == 1, 1],
+            c='b', marker='x', label='1')
+plt.scatter(X_xor[y_xor == -1, 0], X_xor[y_xor == -1, 1],
+            c='r', marker='s', label='-1')
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.show()
+
+# text p.74
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor,y_xor)
+plot_decision_regions(X_xor,y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
+
+# text p.75
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+# text p.76
+
+# RBF カーネルによるSVMのインスタンスを生成（y パラメータを変更）
+svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+svm.fit(X_train_std,y_train)
+plot_decision_regions(X_combined_std,y_combined, classifier=svm,
+test_idx=range(105,150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
 plt.show()
