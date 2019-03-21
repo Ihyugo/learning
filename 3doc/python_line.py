0589e5f8f5ec5eef96cd051eca1b2d435e3f9585
yugo ikuta <yugo1997.10.14@gmail.com>
2019-03-21 21:22:25 +0900

    until 3rd unit

--- 3doc/python_line.py
+++ 3doc/python_line.py
@@ -25,9 +25,10 @@ sc.fit(X_train)
 # 平均と標準偏差を用いて標準化
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
+
 from sklearn.linear_model import Perceptron
 # エポック数4.0、学習率0.1でパーセプトロンのインスタンスを生成
-ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0, shuffle=True)
+ppn = Perceptron(max_iter=60, eta0=0.01, random_state=0, shuffle=True)
 # トレーニングデータをモデルに適合させる
 ppn.fit(X_train_std, y_train)
 #　テストデータで予測を実施
@@ -84,7 +85,7 @@ for c in np.arange(-5, 5):
 
 weights = np.array(weights)
 plt.plot(params, weights[:, 0], label='petal length')
-plt.plot(params, weights[:, 1], linestyle='-', label='petal length')
+plt.plot(params, weights[:, 1], linestyle='-', label='petal width')
 plt.ylabel('weight coefficient')
 plt.xlabel('c')
 plt.legend(loc='upper left')
@@ -100,6 +101,7 @@ plot_decision_regions(X_combined_std,y_combined,classifier=svm,test_idx=range(10
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
+print('Accuracy: %.2f' % svm.score(X_test_std,y_test))
 plt.show()
 
 # text p.71-72
@@ -138,7 +140,7 @@ plt.show()
 # text p.76
 
 # RBF カーネルによるSVMのインスタンスを生成（y パラメータを変更）
-svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+svm = SVC(kernel='rbf', random_state=0, gamma=10.0, C=1.0)
 svm.fit(X_train_std,y_train)
 plot_decision_regions(X_combined_std,y_combined, classifier=svm,
 test_idx=range(105,150))
@@ -147,3 +149,99 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
+
+# text p.83
+
+import matplotlib.pyplot as plt
+import numpy as np
+#ジニ不順度の関数を定義
+def gini(p):
+    return (p)*(1 - (p)) + (1 - p)*(1 - (1 - p))
+
+#　エントロピーの関数を定義
+def entropy(p):
+    return - p*np.log2(p) - (1 - p)*np.log2((1 - p))
+
+#分類誤差の関数を定義
+def error(p):
+    return 1 - np.max([p, 1 - p])
+
+#　確率を表す配列を生成（0から0.99まで0.01刻み)
+x = np.arange(0.0, 1.0, 0.01)
+#　配列の値をもとにエントロピー、分類誤差を計算
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e*0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+#　図の作製を開始
+fig = plt.figure()
+ax = plt.subplot(111)
+#　エントロピー(2種)、ジニ不純度、分類誤差のそれぞれをループ処理
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+                          ['Entropy', 'Entropy (scaled)',
+                           'Gini Inpurity', 'Misclassification Error'],
+                          ['-', '-', '--', '-.'],
+                          ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+#  凡例の設定（中央の上に配置）
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+          ncol=3, fancybox=True, shadow=False)
+#　2本の水平の破線を引く
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+#　横軸の上限/下限の設定
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.xlabel('Impurity Index')
+plt.show()
+
+# text p.84
+
+from sklearn.tree import DecisionTreeClassifier
+#　エントロピーを指標とする決定木のインスタンスを生成
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+#決定木のモデルにトレーニングデータを適合させる
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+# make odt file
+from sklearn.tree import export_graphviz
+export_graphviz(tree, out_file='tree.dot',
+                feature_names=['petal length', 'petal width'])
+
+# text p.88
+
+from sklearn.ensemble import RandomForestClassifier
+# エントロピーを指標とするランダムフォレストのインスタンスを生成
+forest = RandomForestClassifier(criterion='entropy',
+                                n_estimators=10, random_state=1, n_jobs=2)
+#　ランダムフォレストのモデルにトレーニングデータを適合させる
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier=forest,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+print("Accuracy: %.2f" % forest.score(X_train, y_train))
+
+# text p.90
+
+from sklearn.neighbors import KNeighborsClassifier
+# k近傍法のインスタンスを生成
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train) # k近傍法のモデルにトレーニングデータを適合させる
+plot_decision_regions(X_combined_std, y_combined, classifier=knn,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+print("Accuracy: %.2f" % knn.score(X_train_std, y_train))
